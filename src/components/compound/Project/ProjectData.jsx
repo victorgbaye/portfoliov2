ee77c6b4bdb0c04037e32f622a7f4677faaf964c
tomiwadotJS <victorgbayedev@gmail.com>
2023-11-25 23:22:32 +0000

    changes

--- src/components/compound/Project/ProjectData.jsx
+++ src/components/compound/Project/ProjectData.jsx
@@ -10,5 +10,15 @@ export const projectData = [
         productLink: '',
         documentationLink: '',
         githubLink: ''
+    },
+    {
+        image: figplugthumbnail,
+        title: 'Fig Plug',
+        Description: "It's also important to make sure that the font aligns with the overall design system of the website for consistency. Additionally, Figma allows you to import fonts, so you're not limited to the default selection if you have a specific typeface in mind that isn't available in Figma by default.",
+        role: 'Role: Software Developer',
+        productLink: '',
+        documentationLink: '',
+        githubLink: ''
     }
+
 ]
